15235cffcbaef94044f5173a7ffc8de2e14df828
Chris Bentley <chrisrbentley@pm.me>
2022-04-15 16:19:13 -0400

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let rString = '';
 
+    for (let i = 0; i < num; i++) {
+        rString += string
+    }
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    return rString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
