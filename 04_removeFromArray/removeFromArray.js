95e0aaa0f7edc573a67309e6a7f76f7260fad42a
Chris Bentley <chrisrbentley@pm.me>
2022-04-20 17:38:00 -0400

    Completed exercise 4 and began exercise 5.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArr = [];
 
+    array.forEach(element => {
+        if (!args.includes(element))
+            newArr.push(element);
+    });
+    return newArr;
 };
 
 // Do not edit below this line
