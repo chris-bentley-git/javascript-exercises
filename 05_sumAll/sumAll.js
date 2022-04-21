c69495b689a9c54ae9cd08911c51840f25dd173e
Chris Bentley <chrisrbentley@pm.me>
2022-04-21 12:43:26 -0400

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,24 @@
-const sumAll = function(min, max) {
+const sumAll = function(a, b) {
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+
+    if (a < 0 || b < 0 ) {
+        return 'ERROR';
+    }
+
+    if (typeof a == 'string' || typeof b == 'string' || typeof a == 'object' || typeof b == 'object' ) {
+        return 'ERROR';
+    }
+
+
     let sum = 0;
-    for (i = min; i < max + 1; i++); {
+    for (i = a; i <= b; i++) {
         sum += i;
     }
-    return sum
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
