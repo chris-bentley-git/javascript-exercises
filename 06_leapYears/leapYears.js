41180723551d8b394c1fd3dc150871ce60d8f102
Chris Bentley <chrisrbentley@pm.me>
2022-04-22 12:22:40 -0400

    Completed exercise 7 and began exercise 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year / 4) {
+        return true;
+    }
 
+    if (year / 100 && year / 400) {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,27 @@
-const ftoc = function() {
+const ftoc = function(farTemp) {
+  let fconOne = farTemp - 32;
+  let fconTwo = fconOne * .5556;
+
+  // checks for decimal, if so rounds to one decimal.
+  if (fconTwo % 1 != 0) {
+      fconTwo = Math.round(fconTwo * 10) / 10
+  }
+
+  return fconTwo;
+
 
 };
 
-const ctof = function() {
+const ctof = function(celTemp) {
+  let celCon = celTemp * 1.8;
+  let celcon2 = celCon + 32;
 
+  // checks for decimal, rounds.
+  if (celcon2 % 1 != 0) {
+      celcon2 = Math.round(celcon2 * 10) / 10
+  }
+
+  return celcon2;
 };
 
 // Do not edit below this line
