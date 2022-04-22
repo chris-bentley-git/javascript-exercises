68636ca41b244133a1fe5974df28fc7bd4b37734
Chris Bentley <chrisrbentley@pm.me>
2022-04-22 13:47:31 -0400

    Completed exercise 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,19 @@
 const leapYears = function(year) {
-    if (year / 4) {
+    if ((year % 4 == 0) && (year % 100 !== 0) || year % 400 == 0) {
         return true;
+        } else {
+            return false;
+        }
+        
     }
 
-    if (year / 100 && year / 400) {
+    
+
+
+
+/*if (year % 100 == 0 && year % 400 !== 0) {
         return false;
-    }
-};
+    } */
 
 // Do not edit below this line
 module.exports = leapYears;
