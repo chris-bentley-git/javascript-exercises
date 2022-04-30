529ec4f7c5a101d2d7dd513bfb95fef6fbac1e79
Chris Bentley <chrisrbentley@pm.me>
2022-04-30 13:13:58 -0400

    Completed exercise 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let res = a + b;
+  return res;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let res = a - b;
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(function (total, current) {
+    return total + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function (total, current) {
+    return total * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let res = a ** b;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
