cfaa5b5a1d440bbf942e200ee9b5108c17d6d277
Chris Bentley <chrisrbentley@pm.me>
2022-05-01 22:01:29 -0400

    Completed exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function (string) {
-    
+const palindromes = function (str) {
+    let removed = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    let noSpc = removed.replace(/\s/g, '');    //remove spaces
+    let strNew = noSpc.toLowerCase();
+    let spl = strNew.split('');
+    let reversed = spl.reverse();
+    let joined = reversed.join('');
+    if (strNew == joined) {
+        return true; 
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
